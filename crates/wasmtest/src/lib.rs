6c88c052df60d2daed5610e6094d157d4ef1485a
newDINO <new_dino@126.com>
2026-04-22 22:57:05 +0800

    fill sphere example

--- crates/wasmtest/src/lib.rs
+++ crates/wasmtest/src/lib.rs
@@ -1,11 +1,35 @@
 unsafe extern "C" {
     unsafe fn exec(addr: u32, size: u32);
+    unsafe fn put_wood(x: i32, y: i32, z: i32);
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn test() {
+pub extern "C" fn hello_world() {
     let s = "say Hello, world!";
     unsafe {
         exec(s.as_ptr() as u32, s.len() as u32);
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn squre(a: u32) -> u32 {
+    a * a
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn sphere(x: i32, y: i32, z: i32, r: i32) {
+    for offset_x in -r..r {
+        for offset_y in -r..r {
+            for offset_z in -r..r {
+                if offset_x * offset_x + offset_y * offset_y + offset_z * offset_z < r * r {
+                    unsafe { put_wood(x + offset_x, y + offset_y, z + offset_z) };
+                }
+            }
+        }
     }
 }
